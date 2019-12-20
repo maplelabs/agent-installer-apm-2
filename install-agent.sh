6962948003631d187138d6048f39b0df976cac7a
Sharad Jaiswal <sjaiswallmp>
2019-12-20 16:29:08 +0530

    rhel6 changes

--- install-agent.sh
+++ install-agent.sh
@@ -18,19 +18,22 @@ pip install virtualenv --no-index -f pypackages
 virtualenv /opt/sfapm/sfapm-venv/
 source /opt/sfapm/sfapm-venv/bin/activate
 pip install PyYAML --no-index -f pypackages
+pip install pbr --no-index -f pypackages
+pip install traceback2 --no-index -f pypackages
+pip install six --no-index -f pypackages
 #yum install --skip-broken -y sysstat
 \cp -rf collectd /opt/sfapm
 pip install --no-index -f pypackages -r requirements.txt
 \cp -rf configurator-exporter /opt/sfapm
-cp /opt/sfapm/configurator-exporter/init_scripts/configurator.service /etc/systemd/system/configurator.service
-cp /opt/sfapm/collectd/init_scripts/centos7.init /etc/systemd/system/collectd.service
-\cp -rf td-agent-bit /opt/sfapm
-cp /opt/sfapm/td-agent-bit/lib/systemd/system/td-agent-bit.service /etc/systemd/system/td-agent-bit.service
-chmod 777 /opt/sfapm/td-agent-bit/opt/td-agent-bit/bin/td-agent-bit
+cp /opt/sfapm/configurator-exporter/init_scripts/configurator_centos6 /etc/init.d/configurator
+chmod 755 /etc/init.d/configurator
+chkconfig --add configurator
+chkconfig configurator on
+cp /opt/sfapm/collectd/init_scripts/centos6.init /etc/init.d/collectd
+chmod 755 /etc/init.d/collectd
+chkconfig --add collectd
+chkconfig collectd on
 python fluentbit-script/FluentbitConfigurator.py fluentbit-script/props.conf fluentbit-script/input.conf fluentbit-script/reg.csv /opt/sfapm/td-agent-bit/templates
-systemctl daemon-reload
-systemctl enable configurator
-systemctl enable collectd
 #iptables -I INPUT 1 -p tcp -m tcp --dport $CONF_PORT -j ACCEPT
 #iptables-save | sudo tee /etc/sysconfig/iptables
 #service iptables restart
