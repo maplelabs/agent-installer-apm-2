17cc6aef1f4ebe4e6e69f2a90bb112ed9005c21a
krishna-maplelabs <krishna.kulkarni@maplelabs.com>
2020-01-14 13:03:48 +0530

    Add agent configure script to generate and run payload
    Update:
    - Usage: ./agent_configure.sh -h Hostname -e ESHostname -p Project -a Application -i inputFile
    - Modified the input files to have place holders where the script will replace the values
    - When creating input files, add the following in place where values are to be added:
           @h - hostname
           @p - project
           @a - application
           @e - es hostname
    - The script generates a file "payload.json" and runs this file

---
+++ agent_configure.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -h Hostname -e ESHostname -p Project -a Application -i inputFile"
+   echo -e "\t-h Hostname where agent is installed"
+   echo -e "\t-e Elasticsearch Server Name"
+   echo -e "\t-p APM Project Name"
+   echo -e "\t-a APM Application Name"
+   echo -e "\t-i Input file Name"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "h:e:p:a:i:" opt
+do
+   case "$opt" in
+      h ) hostname="$OPTARG" ;;
+      e ) esname="$OPTARG" ;;
+      p ) projectname="$OPTARG" ;;
+      a ) appname="$OPTARG" ;;
+      i ) infile="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$hostname" ] || [ -z "$esname" ] || [ -z "$projectname" ] || [ -z "$appname" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$hostname"
+echo "$esname"
+echo "$projectname"
+echo "$appname"
+sed "s/@h/$hostname/g" $infile > payload.json
+sed -i "s/@e/$esname/g" payload.json
+sed -i "s/@p/$projectname/g" payload.json
+sed -i "s/@a/$appname/g" payload.json
+curl -X POST "localhost:8585/api/config" -d @payload.json -H "Content-Type: application/json"
+
